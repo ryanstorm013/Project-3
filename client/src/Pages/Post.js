3da6f68a2b6e3470b65ee50190fd37027c8c47cc
jtwob <32282322+jtwob@users.noreply.github.com>
2021-03-16 15:06:51 -0700

    UPDATE: added input fields and proper state/update behaviour with backend requests and context

--- client/src/Pages/Post.js
+++ client/src/Pages/Post.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import UserContext from "../Context/UserContext";
 
 const Post = () => {
+  const { userData } = useContext(UserContext);
+  const [form, setForm] = useState({
+    model: "",
+    zip: -1,
+    price: -1,
+    color: "",
+    wheels: -1,
+    ownerId: "",
+  });
+
+  const onChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const submitPost = async (e) => {
+    e.preventDefault();
+    try {
+      const newPost = await axios.post("/api/bikes", form);
+      console.log(newPost);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    setForm({ ...form, ownerId: userData.userId });
+  }, []);
+
   return (
     <div>
-      <h1>New Bike Posts</h1>
+      <form onSubmit={submitPost}>
+        <input onChange={onChange} type="text" name="model" />
+        <input onChange={onChange} type="number" name="zip" />
+        <input onChange={onChange} type="number" name="price" />
+        <input onChange={onChange} type="text" name="color" />
+        <input onChange={onChange} type="number" name="wheels" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
