e1d7aed5f7e828f0bff3a9cbcfbe0b8523f0edb8
Jamly Miller <jamlyyang77@gmail.com>
2021-03-18 19:23:01 -0700

    polishing functionality

--- client/src/Pages/Signup/index.js
+++ client/src/Pages/Signup/index.js
@@ -4,12 +4,15 @@ import Col from "../../Components/Col";
 import Row from "../../Components/Row";
 
 function Signup() {
-  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [DisplayName, setDisplayName] = useState();
+
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log("username is " + username);
+    console.log("email is" + email);
+    console.log("DisplayName is " + DisplayName);
     console.log("password is " + password);
   };
 
@@ -20,17 +23,19 @@ function Signup() {
       </div>
       <form onSubmit={handleSubmit}>
         <Container className="mt-3 px-5">
+
           <Row className="form-group">
             <Col size="12">
               <input
                 className="form-control"
                 type="text"
-                placeholder="Username"
-                name="username"
-                onChange={e => setUsername(e.target.value)}
+                placeholder="email"
+                name="email"
+                onChange={e => setEmail(e.target.value)}
               />
             </Col>
           </Row>
+
           <Row className="form-group">
             <Col size="12">
               <input
@@ -42,12 +47,26 @@ function Signup() {
               />
             </Col>
           </Row>
+
+          <Row className="form-group">
+            <Col size="12">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Displayname"
+                name="Displayname"
+                onChange={e => setDisplayName(e.target.value)}
+              />
+            </Col>
+          </Row>
+
           <button className="btn btn-success" type="submit">
             Submit
           </button>
+
         </Container>
         <Container className="mt-4">
-          <h3>Hello {username}!</h3>
+          <h3>Hello {DisplayName}!</h3>
           <p>I probably shouldn't tell you this, but your password is {password}!</p>
         </Container>
       </form>
