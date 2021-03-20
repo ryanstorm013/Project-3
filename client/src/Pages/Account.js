0acdbfe93fbfdccc95aa7ed1fc6c98379b46db2b
ryanstorm <ryanstorm013@gmail.com>
2021-03-20 11:35:51 -0700

    Updated Account Files

--- client/src/Pages/Account.js
+++ client/src/Pages/Account.js
@@ -1,13 +1,32 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Settings from './Account/AccountPages/settings';
+import MyBikes from './Account/AccountPages/myBikes';
+import { Grid, Row, Col, Container } from "react-bootstrap";
 import SideNav from './Account/SideNav';
 
 
 const Account = () => {
   return (
-    <div>
-      <h1>My Account</h1>
-      <SideNav/>
-    
+    <div style={{color: "grey"}}> 
+      <div className="jumbotron">
+        <h1>My Account</h1>
+        
+      </div>
+      <Container>
+        <Row>
+          <Col>
+            <SideNav/>
+          </Col>
+        <Col sm={8}>
+          <Settings></Settings>
+        </Col>
+          <MyBikes></MyBikes>
+        </Row>
+
+      
+      </Container>
     </div>
     
   );
--- client/src/Pages/Account/AccountPages/myBikes.js
+++ client/src/Pages/Account/AccountPages/myBikes.js
@@ -1,21 +1,25 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
-
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/button";
 
 const MyBikes = () => {
     return (
         <div>
-            <Card style={{ width: '50rem', height: '20rem'}}>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
+            <Card style={{ width: '50rem', height: '30rem'}}>
+            <ListGroup>
+                 <ListGroup.Item>
+                            <Card.Title>My Bikes</Card.Title>
+                        </ListGroup.Item>
+                    <Card.Body> 
+                    <ListGroup.Item>
+                        
+
+                    </ListGroup.Item>
+
                 </Card.Body>
+            </ListGroup>
             </Card>
         </div>
     )
