60a7989d7fb0eee1b98bbcd22c509a2cd763d352
Lauren Hutchinson <lbhutchinson022@gmail.com>
2021-03-20 11:16:36 -0700

    saving auth token to local storage with team

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "../Components/Container";
+import Col from "../Components/Col";
+import Row from "../Components/Row";
+import axios from "axios";
+
 import "../Pages/login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await axios.get(`/api/users/email/${email}`);
+      if (password === user.data.password) {
+        localStorage.setItem("auth-token", user.data._id);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Container className="mt-3 px-5">
+          <Row className="form-group">
+            <Col size="12">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Col>
+          </Row>
+
+          <Row className="form-group">
+            <Col size="12">
+              <input
+                className="form-control"
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Col>
+          </Row>
+
+          <button className="btn btn-success" type="submit">
+            Submit
+          </button>
+        </Container>
+      </form>
     </div>
   );
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 const {
   newUser,
   getOne,
+  getOneByEmail,
   updateOne,
   deleteOne,
 } = require("../controllers/userController");
 
 router.post("/", newUser);
 router.get("/:id", getOne);
+router.get("/email/:email", getOneByEmail);
 router.put("/:id", updateOne);
 router.delete("/:id", deleteOne);
 
