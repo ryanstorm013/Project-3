5ce873a4f4a96996e3f4a0a60c74464465199ddf
James Totah <jtotah@usfca.edu>
2021-03-22 17:30:47 -0700

    UPDATE: logout functionality added

--- client/src/Components/AccountLogout/Logout.js
+++ client/src/Components/AccountLogout/Logout.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useHistory } from "react-router-dom";
 
 const Logout = () => {
+    const history = useHistory();
+
+    useEffect(async () => {
+        localStorage.setItem("auth-token", "");
+        history.push("/");
+    }, [])
     return (
         <div>
             <h2>Logout</h2>
